67020bb8c832a435bedc901087b0a19f1009ef08
PASCAL-Noa <147041489+PASCAL-Noa@users.noreply.github.com>
2026-05-22 21:36:08 +0200

    [Dev][Add] Timer : DeltaTime and FPS

---
+++ Engine/include/Timer.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <chrono>
+
+class Timer
+{
+public:
+    explicit Timer(unsigned int targetFPS = 0);
+    ~Timer() = default;
+
+    void Update();
+
+    float GetDeltaTime() const;
+    float GetFPS() const;
+
+private:
+    std::chrono::high_resolution_clock::time_point m_lastTime;
+    std::chrono::high_resolution_clock::time_point m_lastFpsTime;
+
+    float m_deltaTime;
+    float m_targetDeltaTime;
+    float m_fps;
+    int m_frameCount;
+};
